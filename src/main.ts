ad8499b6b8cc3b40603547cbcac38e66e0622ae3
Almas2002 <almas.zhumakhanov.2002a@gmail.com>
2023-04-22 08:34:58 +0600

    delete heders

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { MyValidationPipe } from './pipes/MyValidation.pipe';
-
+import * as bodyParser from 'body-parser'
 import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors({
-    origin: ['*'],
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   })
   app.use(cookieParser())
@@ -21,6 +21,8 @@ async function bootstrap() {
       'defaultBearerAuth',
     )
     .build();
+  app.use(bodyParser.json({limit: '50mb'}));
+  app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
   const documentation = SwaggerModule.createDocument(app,config)
   SwaggerModule.setup("/api/docs",app,documentation)
   app.useGlobalPipes(new MyValidationPipe());
