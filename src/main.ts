973b9ab636e0772fbe7615393c16e4c6655241d4
Almas2002 <almas.zhumakhanov.2002a@gmail.com>
2023-04-22 07:44:16 +0600

    delete heders

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,7 @@ import { MyValidationPipe } from './pipes/MyValidation.pipe';
 
 import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  app.enableCors({
-    origin:["*"],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  });
+  const app = await NestFactory.create(AppModule,{cors:false})
   app.use(cookieParser())
   const config = new DocumentBuilder()
     .setTitle("Mebel marketplace")
